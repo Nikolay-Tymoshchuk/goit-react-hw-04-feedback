edafba1fc0dcea2c681f600f1c1f7fbfa4e499bf
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-07-01 11:19:23 +0300

    start feedback widget

---
+++ src/components/FeedbackWidget/FeedbackWidget.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+class FeedbackWidget extends Component {
+  render() {
+    return (
+      <div>
+        <h1>FeedbackWidget</h1>
+      </div>
+    );
+  }
+}
+
+export default FeedbackWidget;
---
+++ src/components/FeedbackWidget/FeedbackWidget.styled.jsx
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+export const FeedbackWidget = styled.div`
+  display: flex;
+`;
